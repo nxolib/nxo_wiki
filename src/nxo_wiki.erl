53ebff3a8a5a07cfa1451113de07dff86a1d5d66
Bunny Lushington <bunny@bapi.us>
2021-04-14 11:32:14 -0500

    early functionality

--- src/nxo_wiki.erl
+++ src/nxo_wiki.erl
@@ -1,3 +1,21 @@
 -module(nxo_wiki).
 
--export([]).
+-export([
+         source/0
+        ]).
+
+source() ->
+  Sources = application:get_env(nxo_wiki, md_source, []),
+  compute_source_dirs(Sources, []).
+
+compute_source_dirs([], Acc) ->
+  Acc;
+compute_source_dirs([H|T], Acc) ->
+  Dir = case H of
+          {priv_dir, App, SubDir} ->
+            Priv = code:priv_dir(App),
+            filename:join(Priv, SubDir);
+          {path, Path} ->
+            Path
+        end,
+  compute_source_dirs(T, [Dir, Acc]).
